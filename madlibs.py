06f449fdafad7663b062c436f547b4de8760bba6
Griselda Cortez <griseldacortez0218@gmail.com>
2021-03-11 22:30:25 -0500

    completed the madlibs portion of lab 5 both javascript and python

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = "brick "
+adjective = "huge "
+adverb = "terribly "
+verb = "running "
+
+print("On a tropical island " + str(noun) + ", ")
+print("Underneath a " + str(adjective) + "lava moon. ")
+print("Hangin'  " + str(adverb) + " with the hula dancers, ")
+print(str(verb) + " questions cause' they got all the answers ")
